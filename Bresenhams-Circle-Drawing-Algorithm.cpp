8acb5d8124348368eb9ce58417673885dd6ad049
mamutalib <mohammadmutalib678@gmail.com>
2022-07-28 02:53:54 +0600

    code updated

--- Bresenhams-Circle-Drawing-Algorithm.cpp
+++ Bresenhams-Circle-Drawing-Algorithm.cpp
@@ -1,4 +1,3 @@
-
 /*
     Md. Abdul Mutalib
     B.Sc (Engg.) in CSE
@@ -8,8 +7,6 @@
     Github: https://github.com/mamutalib
 */
 
-
-
 #include <bits/stdc++.h>
 #include<windows.h>
 #include<GL/Glut.h>
@@ -17,6 +14,7 @@
 #include<cstdio>
 #include<cmath>
 #include <iostream>
+
 using namespace std;
 
 #define ll long long
@@ -33,80 +31,67 @@ using namespace std;
 #define pf printf
 #define sc scanf
 
-
-
-
 int x_center, y_center, r;
 
-void plot(int x, int y)
-{
-	glBegin(GL_POINTS);
-	glVertex2i(x + x_center, y + y_center);
-	glEnd();
+void plot(int x, int y) {
+   glBegin(GL_POINTS);
+   glVertex2i(x + x_center, y + y_center);
+   glEnd();
 }
-void plotEightWay(int x,int y)
-{
-	plot(x, y);
-	plot(x, -y);
-	plot(-x, y);
-	plot(-x, -y);
-	plot(y, x);
-	plot(-y, x);
-	plot(y, -x);
-	plot(-y, -x);
+void plotEightWay(int x, int y) {
+   plot(x, y);
+   plot(x, -y);
+   plot(-x, y);
+   plot(-x, -y);
+   plot(y, x);
+   plot(-y, x);
+   plot(y, -x);
+   plot(-y, -x);
 }
 
-void brasenhamCircle()
-{
-	int x = 0;
-	int y = r;
-	float decision = 3 - (2*r);
-	plotEightWay(x, y);
+void brasenhamCircle() {
+   int x = 0;
+   int y = r;
+   float decision = 3 - (2 * r);
+   plotEightWay(x, y);
 
-	while (y > x)
-	{
-		if (decision <= 0)
-		{
-			decision = decision + (4 * x) + 6;
-		}
-		else
-		{
-			decision = decision + (4 * x) - (4 * y) + 10;
-			y = y - 1;
-		}
-		x++;
-		plotEightWay(x, y);
-	}
+   while (y > x) {
+      if (decision <= 0) {
+         decision = decision + (4 * x) + 6;
+      } else {
+         decision = decision + (4 * x) - (4 * y) + 10;
+         y = y - 1;
+      }
+      x++;
+      plotEightWay(x, y);
+   }
 
 }
 
-void display(void)
-{
-	brasenhamCircle();
+void display(void) {
+   brasenhamCircle();
 
-	glFlush();
+   glFlush();
 }
-void init(void)
-{
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+void init(void) {
+   glClearColor(1.0, 1.0, 1.0, 0.0);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(0.0, 640.0, 0.0, 480.0);
 }
 
-int main(int argc, char** argv) {
-	cout << "Enter the center co-ordinates and radius:\n";
-	cin >> x_center >> y_center >> r;
+int main(int argc, char ** argv) {
+   cout << "Enter the center co-ordinates and radius:\n";
+   cin >> x_center >> y_center >> r;
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(100, 150);
-	glutCreateWindow("Bresenham Circle Drawing Algorithm");
-	init();
-	glutDisplayFunc(display);
-	glutMainLoop();
+   glutInit( & argc, argv);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowSize(640, 480);
+   glutInitWindowPosition(100, 150);
+   glutCreateWindow("Bresenham Circle Drawing Algorithm");
+   init();
+   glutDisplayFunc(display);
+   glutMainLoop();
 
-	return 0;
+   return 0;
 }
-
