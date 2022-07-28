531a274b950dd80ad47f51aa9ea6e0e6b41bf4bc
mamutalib <mohammadmutalib678@gmail.com>
2022-07-29 00:21:32 +0600

    error fixed

--- Bresenhams-Line-Drawing-Algorithm.cpp
+++ Bresenhams-Line-Drawing-Algorithm.cpp
@@ -10,7 +10,6 @@
 
 
 
-
 #include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
@@ -29,67 +28,67 @@
 #define pf printf
 #define sc scanf
 
-float x=0,y,x2,y2,m,i,j,p;
-int dx=0,dy=0,r;
 
 
+float x1, y1, x2, y2, m, i, j, p;
+int dx = 0, dy = 0;
 void display(void) {
-    glClear (GL_COLOR_BUFFER_BIT);
-    glEnd();
+  glClear(GL_COLOR_BUFFER_BIT);
+  glEnd();
 
-    glColor3f (0.0, 1.0, 0.0);
-    glBegin(GL_POINTS);
-    p=1-r;
-    while((x<=y)){
-        if(p<0){
-          x=x+1;
-          y=y;
-            printf("%0.2f %0.2f\n",x,y);
-            p=p+(2*x)+1;
-            }
-
-
-            else{
-            x=x+1;
-            y=y-1;
-            printf("%0.2f %0.2f\n",x,y);
-            p=p+(2*x)+1-(2*y);
-            }
-            glVertex3f (((x/100)), ((y/100)), 0.0);
-            glVertex3f (((y/100)), ((x/100)), 0.0);
-            glVertex3f ((-(x/100)), (-(y/100)), 0.0);
-            glVertex3f ((-(x/100)), ((y/100)), 0.0);
-            glVertex3f (((x/100)), (-(y/100)), 0.0);
-            glVertex3f (((y/100)), (-(x/100)), 0.0);
-            glVertex3f ((-(y/100)), (-(x/100)), 0.0);
-            glVertex3f ((-(y/100)), ((x/100)), 0.0);
-        }
-    glEnd();
-
-    glFlush ();
+  glColor3f(0.0, 1.0, 0.0);
+  glBegin(GL_POINTS);
+  p = (2 * dy) - dx;
+  for (i = x1, j = y1; i <= x2, j <= y2;) {
+    if (p >= 0) {
+      i = i + 1;
+      j = j + 1;
+      if ((i > x2) || (j > y2)) {
+        break;
+      }
+      printf("%0.2f %0.2f\n", i, j);
+      glVertex3f((i / 100), (j / 100), 0.0);
+      p = p + (2 * dy) - (2 * dx);
+    } else if (p < 0) {
+      i = i + 1;
+      if ((i > x2) || (j > y2)) {
+        break;
+      }
+      printf("%0.2f %0.2f\n", i, j);
+      glVertex3f((i / 100), (j / 100), 0.0);
+      p = p + (2 * dy);
+    }
+  }
+  glEnd();
+  glFlush();
 }
 
-void init (void) {
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+void init(void) {
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
-int main(int argc, char** argv){
 
-    printf("Enter radius: ");
-    scanf("%d",&r);
-    y=r;
+int main(int argc, char ** argv) {
+
+  printf("Enter first point: ");
+  scanf("%f %f", & x1, & y1);
+  printf("Enter second point: ");
+  scanf("%f %f", & x2, & y2);
+  dx = x2 - x1;
+  dy = y2 - y1;
+  glutInit( & argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Bresenham Line Drawing Algorithm");
+  init();
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
 
 
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("Midpoint Circle Drawing Algorithm");
-    init ();
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
 }
